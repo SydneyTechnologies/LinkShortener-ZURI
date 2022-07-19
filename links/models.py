69910464af8ab6c0694f6375b0dc040b2bbac198
sydney <sydney.idundun@gmail.com>
2022-07-19 11:42:42 +0200

    made all the necessary changes to view the active links through the links model class, using the Managers class

--- links/models.py
+++ links/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from managers import ActiveLinkManager
 
 # Create your models here.
 
@@ -8,6 +9,8 @@ class Link(models.Model):
     target_url = models.URLField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=200, blank=True, null=True)
     identifier = models.SlugField(blank=True, unique=True, max_length=20)
+    objects = models.Manager()
+    public = ActiveLinkManager()
     author = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
     created_date = models.DateTimeField()
